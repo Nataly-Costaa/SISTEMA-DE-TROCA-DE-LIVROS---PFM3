06598b11fda9f2f43c5fb71d9f1457a5f8d7139f
Nataly-Costaa <natalynaty653@gmail.com>
2025-03-01 12:23:31 -0300

    :sparkles: feat: Tabela Usuário

--- book exchange system.sql
+++ book exchange system.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE exchange_books;
 USE exchange_books;
+
+CREATE TABLE user (
+	id_user INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(80) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    location VARCHAR(100),
+    phone_number VARCHAR(15),
+    password VARCHAR(30),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
