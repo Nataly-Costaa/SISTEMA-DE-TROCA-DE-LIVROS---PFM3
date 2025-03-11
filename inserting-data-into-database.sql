3646a883ff1e87bd7e8c5c737247861ece47ca04
tereza <brunaaasilvaaa261@gmail.com>
2025-03-11 20:29:13 -0300

    :sparkles: feat: Inserindo as procedures

--- inserting-data-into-database.sql
+++ inserting-data-into-database.sql
@@ -1,3 +1,174 @@
+-- inserindo dados
+-- criando trigerrs para tabelas de historico
+DELIMITER $$
+CREATE TRIGGER insert_exchange_history
+AFTER INSERT ON exchange
+FOR EACH ROW
+BEGIN
+-- insere automaticamente no historico de trocas
+INSERT INTO exchange_history (id_exchange, status, created_at, updated_at)
+    VALUES (NEW.id_exchange, 'pendente', NOW(), NOW());
+END $$
+
+DELIMITER $$
+CREATE TRIGGER insert_owner_history
+AFTER INSERT ON book
+FOR EACH ROW
+BEGIN
+-- insere o historico do dono de livro
+INSERT INTO owner_history(id_book, id_user, date_of_acquisition, status, created_at, updated_at)
+VALUES(NEW.id_book, NEW.id_user, NOW(), 'Em posse', NOW(), NOW());
+END $$
+
+DELIMITER $$
+CREATE TRIGGER insert_owner_history_on_update
+AFTER UPDATE ON book
+FOR EACH ROW
+BEGIN
+IF NEW.id_user != OLD.id_user THEN
+-- insere o histórico de posse do livro para o novo dono
+INSERT INTO owner_history(id_book, id_user, date_of_acquisition, status, created_at, updated_at)
+VALUES(NEW.id_book, NEW.id_user, NOW(), 'Em posse', NOW(), NOW());
+-- atualiza o status do dono anterior para "Trocado"
+UPDATE owner_history
+SET status = 'Trocado', end_date = NOW(), updated_at = NOW()
+WHERE id_book = NEW.id_book AND id_user = OLD.id_user AND end_date IS NULL;
+END IF;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE insert_user(
+IN p_first_name VARCHAR(30),
+IN p_last_name VARCHAR(80),
+IN p_email VARCHAR(100),
+IN p_location VARCHAR(100),
+IN p_phone_number VARCHAR(15),
+IN p_password VARCHAR(30)
+)
+BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN
+ROLLBACK;
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = "Erro ao inserir dados de user";    
+END;
+START TRANSACTION;
+INSERT INTO user (first_name, last_name, email, location, phone_number, password, created_at, updated_at)
+VALUES (p_first_name, p_last_name, p_email, p_location, p_phone_number, p_password, NOW(), NOW());
+    COMMIT;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE insert_book(
+IN p_id_user INT, 
+IN p_title VARCHAR(100), 
+IN p_author VARCHAR(60),
+IN p_category VARCHAR(50),
+IN p_preservation_status ENUM('Novo','Usado','Desgastado'), 
+IN p_publication_date DATE,
+IN p_synopsis TEXT,
+IN p_user_review TEXT,
+IN p_publisher VARCHAR(100), 
+IN p_available BOOLEAN
+)
+BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN
+ROLLBACK;
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = "Erro ao inserir dados de livro";    
+END;
+START TRANSACTION;
+INSERT INTO book (id_user, title, author, category, preservation_status, publication_date, synopsis, user_review, publisher, available, created_at, updated_at)
+VALUES(p_id_user, p_title, p_author, p_category,p_preservation_status, p_publication_date,
+p_synopsis,p_user_review,p_publisher, p_available, NOW(), NOW());
+COMMIT;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE insert_exchange(
+IN p_offered_book_id INT,
+IN p_received_book_id INT,
+IN p_offerer_user_id INT,
+IN p_receiver_user_id INT,
+IN p_form_of_swap ENUM ("envio","presencial"))
+BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN
+ROLLBACK;
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = "Erro ao inserir dados de troca";    
+END;
+START TRANSACTION;
+INSERT INTO exchange(offered_book_id, received_book_id, offerer_user_id, receiver_user_id, form_of_swap, created_at, updated_at)
+VALUES(p_offered_book_id, p_received_book_id, p_offerer_user_id, p_receiver_user_id, p_form_of_swap, NOW(), NOW());
+COMMIT;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE insert_user_rating(
+IN p_rater_user_id INT,
+IN p_rated_user_id INT,
+IN p_score INT,
+IN p_comment TEXT
+)
+BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+BEGIN
+ROLLBACK;
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = 'Erro ao inserir dados da avaliação de usuário';    
+END;
+START TRANSACTION;
+INSERT INTO user_rating (rater_user_id, rated_user_id, score, comment, created_at, updated_at)
+VALUES (p_rater_user_id, p_rated_user_id, p_score, p_comment, NOW(), NOW());
+COMMIT;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE insert_literary_group(
+IN p_group_name VARCHAR(100),
+IN p_description TEXT,
+IN p_id_book INT
+)
+BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+BEGIN
+ROLLBACK;
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = 'Erro ao inserir dados do grupo literário';    
+END;
+START TRANSACTION;
+INSERT INTO literary_group (group_name, description, id_book, created_at, updated_at)
+VALUES (p_group_name, p_description, p_id_book, NOW(), NOW());
+COMMIT;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE insert_user_group(
+IN p_id_user INT,
+IN p_id_group INT
+)
+BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+BEGIN
+ROLLBACK;
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = 'Erro ao inserir dados na tabela intermediária de usuário e grupo';    
+END;
+START TRANSACTION;
+INSERT INTO user_group (id_user, id_group, created_at, updated_at)
+VALUES (p_id_user, p_id_group, NOW(), NOW());
+COMMIT;
+END $$
+DELIMITER ;
+
 -- inserindo dados dos usuarios
 CALL insert_user('Lucas', 'Mendes', 'lucas.mendes@email.com', 'Brasília', '6677889900', 'senha105');
 CALL insert_user('Mariana', 'Ferreira', 'mariana.ferreira@email.com', 'Belo Horizonte', '7788990011', 'senha106');
